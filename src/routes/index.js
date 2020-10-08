56d629cb9c24e626bbcc8b3884dc898d8d00da04
marcos.s <marcos.s@dasa.com.br>
2020-10-08 17:50:35 -0300

    config microfrontend

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import Details from '../containers/Details';
 import PageNotFound from '../containers/PageNotFound';
 
 const Routes = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="marvel">
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/details/:characterId' component={Details} />
